2783fb066f25db7c8ffd2099012bb19b31b49afe
bpvand <127121513+bpvand@users.noreply.github.com>
2024-08-28 15:30:31 +0200

    Delete PyTimeVar/timevarmodels directory

--- PyTimeVar/timevarmodels/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "0.0.1"
-
-from .bhpfilter.bHP import BoostedHP
-from .locallinear.LLR import LocalLinearRegression
-from .kalman.kalman import Kalman
-from .gas.tGAS import tGAS
-import timevarmodels.datasets as datasets
--- PyTimeVar/timevarmodels/datasets/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Dataset module for TimeVarModels package.
-"""
-
-from . import gold, temperature, co2, usd
-
-__all__ = ["gold", "temperature, co2, usd"]
