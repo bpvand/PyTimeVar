28f48193e92fee3c338332f1cd4ba218062cc5db
bpvand <zeker.bernhard@gmail.com>
2024-08-30 11:27:56 +0200

    Update Kalman for regressors

--- PyTimeVar/PyTimeVar/gas/GAS.py
+++ PyTimeVar/PyTimeVar/gas/GAS.py
@@ -7,13 +7,14 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import pandas as pd
 
+
 class GAS:
     """
     Class for performing GAS filtering.
-    
+
     """
-    
-    def __init__(self, vY: np.ndarray, mX: np.ndarray, method: str='none'):
+
+    def __init__(self, vY: np.ndarray, mX: np.ndarray, method: str = 'none'):
         self.vY = vY.flatten()
         self.mX = mX
         self.n = len(vY)
@@ -23,32 +24,38 @@ class GAS:
         self.params = None
 
     def fit(self):
-        
-        if self.method=='none':
+
+        if self.method == 'none':
             print('Warning: no filter method is specified. A t-GAS filter is computed.')
             self.method = 'student'
-    
-        # set initial values 
+
+        # set initial values
         dnInitial = int(np.ceil(self.n / 10))
-        vbeta0 = np.linalg.inv((self.mX[:dnInitial, :]).T @ (self.mX[:dnInitial, :])) @ ((self.mX[:dnInitial, :]).T @ self.vY[:dnInitial])
-        LB = np.concatenate(([0.001], -10 * np.ones(self.n_est), -np.ones(self.n_est), -50 * np.ones(self.n_est)))
-        UB = np.concatenate(([100], 10 * np.ones(self.n_est), np.ones(self.n_est), 50 * np.ones(self.n_est)))
-    
+        vbeta0 = np.linalg.inv((self.mX[:dnInitial, :]).T @ (self.mX[:dnInitial, :])) @ (
+            (self.mX[:dnInitial, :]).T @ self.vY[:dnInitial])
+        LB = np.concatenate(([0.001], -10 * np.ones(self.n_est), -
+                            np.ones(self.n_est), -50 * np.ones(self.n_est)))
+        UB = np.concatenate(([100], 10 * np.ones(self.n_est),
+                            np.ones(self.n_est), 50 * np.ones(self.n_est)))
+
         options = {'maxfun': 5E5}
-    
+
         start_time = time.time()
-    
+
         if self.method == 'gaussian':  # MLE by Gaussian-GAS
-        
+
             vdelta0 = 1
-            vtheta0 = np.concatenate([0.1*np.ones(self.n_est),  0.1*np.ones(self.n_est),0.1*np.ones(self.n_est)])
+            vtheta0 = np.concatenate(
+                [0.1*np.ones(self.n_est),  0.1*np.ones(self.n_est), 0.1*np.ones(self.n_est)])
             vgamma0 = np.concatenate([[vdelta0], vtheta0])
-        
-            fgGAS_lh = lambda vpara: -self.construct_likelihood(vbeta0, vpara)
-            result = minimize(fgGAS_lh, vgamma0, bounds=list(zip(LB, UB)), options=options)
+
+            def fgGAS_lh(vpara): return - \
+                self.construct_likelihood(vbeta0, vpara)
+            result = minimize(fgGAS_lh, vgamma0, bounds=list(
+                zip(LB, UB)), options=options)
             vparaHat_gGAS = result.x
             vparaHat = vparaHat_gGAS
-    
+
             # construct betat estimate
             vbetaNow = vbeta0
             mBetaHat_gGAS = np.zeros((self.n_est, self.n))
@@ -56,31 +63,35 @@ class GAS:
             vomegaHat_gGAS = vthetaHat_gGAS[:self.n_est]
             mBHat_gGAS = vthetaHat_gGAS[self.n_est:2*self.n_est]
             mAHat_gGAS = vthetaHat_gGAS[2*self.n_est:]
-    
+
             for id in range(self.n):
                 vxt = self.mX[id, :].reshape(-1, 1)
                 yt = self.vY[id]
-    
+
                 mNablat = vxt * (yt - vbetaNow.T @ vxt)
-                vbetaNow = vomegaHat_gGAS + mBHat_gGAS * vbetaNow + mAHat_gGAS * mNablat.squeeze()
-    
+                vbetaNow = vomegaHat_gGAS + mBHat_gGAS * \
+                    vbetaNow + mAHat_gGAS * mNablat.squeeze()
+
                 mBetaHat_gGAS[:, id] = vbetaNow
-    
+
             mBetaHat = mBetaHat_gGAS.T
-    
+
         elif self.method == 'student':  # MLE by t-GAS
             LB = np.concatenate(([0.01], LB))
             UB = np.concatenate(([200], UB))
-            
+
             vdelta0 = np.array([10, 1])
-            vtheta0 = np.concatenate([np.ones(self.n_est), -0.1 * np.ones(self.n_est), 0.1 * np.ones(self.n_est)])
+            vtheta0 = np.concatenate(
+                [np.ones(self.n_est), -0.1 * np.ones(self.n_est), 0.1 * np.ones(self.n_est)])
             vgamma0 = np.concatenate([vdelta0, vtheta0])
-            
-            ftGAS_lh = lambda vpara: -self.construct_likelihood(vbeta0, vpara)
-            result = minimize(ftGAS_lh, vgamma0, bounds=list(zip(LB, UB)), options=options)
+
+            def ftGAS_lh(vpara): return - \
+                self.construct_likelihood(vbeta0, vpara)
+            result = minimize(ftGAS_lh, vgamma0, bounds=list(
+                zip(LB, UB)), options=options)
             vparaHat_tGAS = result.x
             vparaHat = vparaHat_tGAS
-    
+
             # construct betat estimate
             vbetaNow = vbeta0
             mBetaHat_tGAS = np.zeros((self.n_est, self.n))
@@ -90,47 +101,49 @@ class GAS:
             vomegaHat_tGAS = vthetaHat_tGAS[:self.n_est]
             mBHat_tGAS = vthetaHat_tGAS[self.n_est:2*self.n_est]
             mAHat_tGAS = vthetaHat_tGAS[2*self.n_est:]
-    
+
             for id in range(self.n):
                 vxt = self.mX[id, :].reshape(-1, 1)
                 yt = self.vY[id]
-    
-                temp1 = (1 + dnuHat_tGAS**(-1)) * (1 + dnuHat_tGAS**(-1) * ((yt - vbetaNow.T @ vxt) / dsigmauHat_tGAS)**2)**(-1)
-                mNablat = (1 + dnuHat_tGAS)**(-1) * (3 + dnuHat_tGAS) * temp1 * vxt * (yt - vbetaNow.T @ vxt)
-                vbetaNow = vomegaHat_tGAS + mBHat_tGAS * vbetaNow + mAHat_tGAS * mNablat.squeeze()
-    
+
+                temp1 = (1 + dnuHat_tGAS**(-1)) * (1 + dnuHat_tGAS**(-1)
+                                                   * ((yt - vbetaNow.T @ vxt) / dsigmauHat_tGAS)**2)**(-1)
+                mNablat = (1 + dnuHat_tGAS)**(-1) * (3 + dnuHat_tGAS) * \
+                    temp1 * vxt * (yt - vbetaNow.T @ vxt)
+                vbetaNow = vomegaHat_tGAS + mBHat_tGAS * \
+                    vbetaNow + mAHat_tGAS * mNablat.squeeze()
+
                 mBetaHat_tGAS[:, id] = vbetaNow
-    
+
             mBetaHat = mBetaHat_tGAS.T
-    
+
         self.betas, self.params = mBetaHat, vparaHat
         print(f"Time taken: {time.time() - start_time:.2f} seconds")
         return mBetaHat, vparaHat
-    
-    
+
     def construct_likelihood(self, vbeta0, vpara):
-        # set initial values 
+        # set initial values
         vbetaNow = vbeta0
         lhVal = 0
-    
+
         if self.method == 'gaussian':
             dsigmau = vpara[0]
             vtheta = vpara[1:]
             vomega = vtheta[:self.n_est]
             mB = vtheta[self.n_est:2*self.n_est]
             mA = vtheta[2*self.n_est:]
-    
+
             for id in range(self.n):
                 vxt = self.mX[id, :].reshape(-1, 1)
                 yt = self.vY[id]
-    
+
                 lhVal += ((yt - vbetaNow.T @ vxt) / dsigmau)**2
-    
+
                 mNablat = vxt * (yt - vbetaNow.T @ vxt)
                 vbetaNow = vomega + mB * vbetaNow + mA * mNablat.squeeze()
-    
+
             lhVal = -np.log(dsigmau) - 0.5 * lhVal / self.n
-    
+
         elif self.method == 'student':
             dnu = vpara[0]
             dsigmau = vpara[1]
@@ -138,32 +151,36 @@ class GAS:
             vomega = vtheta[:self.n_est]
             mB = vtheta[self.n_est:2*self.n_est]
             mA = vtheta[2*self.n_est:]
-    
+
             for id in range(self.n):
                 vxt = self.mX[id, :].reshape(-1, 1)
                 yt = self.vY[id]
-    
+
                 lhVal += ((yt - vbetaNow.T @ vxt) / dsigmau)**2
-    
-                temp1 = (1 + dnu**(-1)) * (1 + dnu**(-1) * ((yt - vbetaNow.T @ vxt) / dsigmau)**2)**(-1)
-                mNablat = (1 + dnu)**(-1) * (3 + dnu) * temp1 * vxt * (yt - vbetaNow.T @ vxt)
+
+                temp1 = (1 + dnu**(-1)) * (1 + dnu**(-1) *
+                                           ((yt - vbetaNow.T @ vxt) / dsigmau)**2)**(-1)
+                mNablat = (1 + dnu)**(-1) * (3 + dnu) * \
+                    temp1 * vxt * (yt - vbetaNow.T @ vxt)
                 vbetaNow = vomega + mB * vbetaNow + mA * mNablat.squeeze()
-    
-            lhVal = -0.5 * (dnu + 1) * lhVal / self.n + gammaln((dnu + 1) / 2) - gammaln(dnu / 2) - 0.5 * np.log(np.pi * dnu) - np.log(dsigmau)
-    
+
+            lhVal = -0.5 * (dnu + 1) * lhVal / self.n + gammaln((dnu + 1) / 2) - \
+                gammaln(dnu / 2) - 0.5 * np.log(np.pi * dnu) - np.log(dsigmau)
+
         return lhVal
+
     def _format_x_axis(self, ax, date_list):
-          def format_func(x, pos):
-              if len(date_list) > 0:
-                  index = int(x)
-                  if 0 <= index < len(date_list):
-                      date = date_list[index]
-                      if date_list[-1] - date_list[0] > pd.Timedelta(days=730):
-                          return date.strftime('%Y')
-                      else:
-                          return date.strftime('%b %Y')
-              return ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
-    
+        def format_func(x, pos):
+            if len(date_list) > 0:
+                index = int(x)
+                if 0 <= index < len(date_list):
+                    date = date_list[index]
+                    if date_list[-1] - date_list[0] > pd.Timedelta(days=730):
+                        return date.strftime('%Y')
+                    else:
+                        return date.strftime('%b %Y')
+            return ax.xaxis.set_major_formatter(plt.FuncFormatter(format_func))
+
     def _generate_dates(self, length, start_date, end_date):
         # Calculate the total duration in days
         total_days = (end_date - start_date).days
@@ -172,7 +189,7 @@ class GAS:
         dates = pd.date_range(start=start_date, end=end_date, periods=length)
 
         return dates
-    
+
     def plot(self, date_range=None):
         """
         Plot the beta coefficients over a normalized x-axis from 0 to 1 or over a date range.
@@ -184,13 +201,16 @@ class GAS:
             axs = [axs]
 
         if date_range:
-            start_date, end_date = [datetime.strptime(date, "%Y-%m-%d") for date in date_range]
-            x_vals = self._generate_dates(self.betas.shape[0], start_date, end_date)
+            start_date, end_date = [datetime.strptime(
+                date, "%Y-%m-%d") for date in date_range]
+            x_vals = self._generate_dates(
+                self.betas.shape[0], start_date, end_date)
         else:
             x_vals = np.linspace(0, 1, self.betahat.shape[1])
-            
+
         for i in range(self.n_est):
-            axs[i].plot(x_vals, self.betas[:,i], label=r'$\beta_{{{:2d}}}$'.format(i+1))
+            axs[i].plot(x_vals, self.betas[:, i],
+                        label=r'$\beta_{{{:2d}}}$'.format(i+1))
             axs[i].set_title(r'$\beta_{{{:2d}}}$'.format(i+1))
             axs[i].set_xlabel("Date" if date_range else "t/n")
             axs[i].set_ylabel(r"$\beta$ Value")
@@ -201,6 +221,7 @@ class GAS:
         plt.tight_layout()
         plt.show()
 
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     # Parameter specification for simulating data
@@ -209,38 +230,42 @@ if __name__ == "__main__":
     burnin = 20
     sigmau = 0.1
     mA = np.array([[0.3, 0.1], [0.1, 0.2]])
-    
+
     # Simulate data
-    beta0x = lambda x: -4 * x**3 + 9 * x**2 - 6 * x + 2
-    beta1x = lambda x: 1.5 * np.exp(-10 * (x - 0.2)**2) + 1.6 * np.exp(-8 * (x - 0.8)**2)
-    beta2x = lambda x: -0.5 * x - 0.5 * np.exp(-5 * (x - 0.8)**2)
+    def beta0x(x): return -4 * x**3 + 9 * x**2 - 6 * x + 2
+    def beta1x(x): return 1.5 * np.exp(-10 * (x - 0.2)**2) + \
+        1.6 * np.exp(-8 * (x - 0.8)**2)
+
+    def beta2x(x): return -0.5 * x - 0.5 * np.exp(-5 * (x - 0.8)**2)
     trend = np.arange(1, n + 1) / n
     mbeta = np.vstack([beta0x(trend), beta1x(trend), beta2x(trend)]).T
-    
-    mXi = multivariate_normal.rvs(mean=np.zeros(2), cov=np.eye(2), size=n + burnin).T
+
+    mXi = multivariate_normal.rvs(mean=np.zeros(
+        2), cov=np.eye(2), size=n + burnin).T
     mX = np.zeros((2, n + burnin))
     for i in range(1, n + burnin):
         mX[:, i] = mA @ mX[:, i - 1] + mXi[:, i]
     mX = np.hstack([np.ones((n, 1)), mX[:, -n:].T])
     vu = sigmau * t.rvs(nu, size=n)
     vy = np.sum(mbeta * mX, axis=1) + vu
-    
+
     # MLE by Gaussian-GAS
     gGAS = GAS(vy, mX, method='gaussian')
     mBetaHat_gGAS, vparaHat_gGAS = gGAS.fit()
-    
+
     # MLE by t-GAS
     tGAS = GAS(vy, mX, method='student')
     mBetaHat_tGAS, vparaHat_tGAS = tGAS.fit()
-    
+
     # Make plots
     for id2 in range(mX.shape[1]):
         plt.figure(figsize=(12, 6))
         plt.plot(mbeta[:, id2], '-k', linewidth=3, label='true')
-        plt.plot(mBetaHat_gGAS[:, id2], '-.r', linewidth=3, label='$\\mathcal{N}$-GAS')
+        plt.plot(mBetaHat_gGAS[:, id2], '-.r',
+                 linewidth=3, label='$\\mathcal{N}$-GAS')
         plt.plot(mBetaHat_tGAS[:, id2], '--b', linewidth=3, label='$t$-GAS')
         plt.grid(which='minor')
         plt.legend(fontsize=20, loc='best', frameon=False)
         plt.gca().tick_params(axis='both', which='major', labelsize=20)
-    
+
         plt.show()
--- PyTimeVar/replicateSection3.py
+++ PyTimeVar/replicateSection3.py
@@ -4,14 +4,19 @@ Section 3: illustration of code on Temperature dataset
 
 '''
 # Download data
+from PyTimeVar.datasets import herding
+from PyTimeVar import GAS
+from PyTimeVar import Kalman
+from PyTimeVar import BoostedHP
+from PyTimeVar import LocalLinear
 from PyTimeVar.datasets import temperature
 import numpy as np
-data = temperature.load(regions=['World'],start_date='1961-01-01', end_date='2023-01-01')
+data = temperature.load(
+    regions=['World'], start_date='1961-01-01', end_date='2023-01-01')
 vY = data.values
 X = np.ones_like(vY)
 
 # illustrate LLR
-from PyTimeVar import LocalLinear
 model = LocalLinear(vY, X)
 res = model.fit()
 
@@ -22,37 +27,37 @@ res.summary()
 res.betas()
 
 # plot trend and data
-res.plot_actual_vs_predicted(date_range=["1980-01-01","2000-01-01"])
+res.plot_actual_vs_predicted(date_range=["1980-01-01", "2000-01-01"])
 
 # plot confidence bands using LBWB
-cb = res.plot_confidence_bands(bootstrap_type='LBWB', date_range=['1980-01-01', '2000-01-01'], Gsubs=None)
+cb = res.plot_confidence_bands(bootstrap_type='LBWB', date_range=[
+                               '1980-01-01', '2000-01-01'], Gsubs=None)
 
 # illustrate boosted HP filter
-from PyTimeVar import BoostedHP
 bHPmodel = BoostedHP(vY, dLambda=1600, iMaxIter=100)
-bHPtrend, bHPresiduals = bHPmodel.fit(boost=True, stop="adf", dAlpha=0.05, verbose=False)
+bHPtrend, bHPresiduals = bHPmodel.fit(
+    boost=True, stop="adf", dAlpha=0.05, verbose=False)
 bHPmodel.summary()
 bHPmodel.plot()
 
-from PyTimeVar import Kalman
 kalmanmodel = Kalman(vY=vY)
 smooth_trend = kalmanmodel.fit('smoother')
 kalmanmodel.summary()
 kalmanmodel.plot()
 
-from PyTimeVar import GAS
 gasmodel = GAS(vY, X, 'student')
 tGAStrend, tGASparams = gasmodel.fit()
 gasmodel.plot(date_range=['1980-01-01', '2000-01-01'])
 
-from PyTimeVar.datasets import herding
 herd_data = herding.load(start_date='2015-01-05', end_date='2022-01-05')
 vY = herd_data[['CSAD_AVG']].values
-mX = herd_data[['AVG_RTN','RTN_ABS', 'RTN_2', 'Intercept']].values
-print(vY.shape)
-print(mX.shape)
+mX = herd_data[['AVG_RTN', 'RTN_ABS', 'RTN_2', 'Intercept']].values
 
-gasmodel = GAS(vY, mX, 'student')
-tGAStrend, tGASparams = gasmodel.fit()
-gasmodel.plot(date_range=['2015-01-05', '2022-01-05'])
+# gasmodel = GAS(vY, mX, 'student')
+# tGAStrend, tGASparams = gasmodel.fit()
+# gasmodel.plot(date_range=['2015-01-05', '2022-01-05'])
 
+kalmanmodel = Kalman(vY=vY, regressors=mX)
+smooth_trend = kalmanmodel.fit('smoother')
+kalmanmodel.summary()
+kalmanmodel.plot(date_range=['2015-01-05', '2022-01-05'])
