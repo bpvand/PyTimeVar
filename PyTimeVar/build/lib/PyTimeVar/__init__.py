fe02f7b7ef3fe753711cb49a13883b4de9bd9818
bpvand <zeker.bernhard@gmail.com>
2024-09-03 14:28:28 +0200

    Update to open source

---
+++ PyTimeVar/build/lib/PyTimeVar/__init__.py
@@ -0,0 +1,8 @@
+__version__ = "0.0.1"
+
+from .bhpfilter.bHP import BoostedHP
+from .locallinear.LLR import LocalLinear
+from .kalman.kalman import Kalman
+from .powerlaw.pwr import PowerLaw
+from .gas.GAS import GAS
+import PyTimeVar.datasets as datasets
---
+++ PyTimeVar/build/lib/tests/test_timevarmodels.py
@@ -0,0 +1,4 @@
+import timevarmodels as tvm
+
+def test_version():
+  assert tvm.__version__ == '0.0.1'
