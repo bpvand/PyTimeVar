b0e3a971bb5f4a56949235355fd4e23b83671e12
bpvand <zeker.bernhard@gmail.com>
2024-09-03 14:37:06 +0200

    Upload License

--- PyTimeVar/setup.py
+++ PyTimeVar/setup.py
@@ -4,5 +4,6 @@ setuptools.setup(
     name='PyTimeVar',
     version="0.0.1",
     python_requires=">=3.9",
-    packages=['PyTimeVar', 'tests']
+    packages=['PyTimeVar', 'tests'],
+    license_files=('LICENSE.txt',),
 )
