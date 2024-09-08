2d2e571a810f5995176573ef4206533922212ca2
Mxuann7 <122456366+Mxuann7@users.noreply.github.com>
2024-09-08 12:53:43 +0200

    Update bHP.py

--- PyTimeVar/PyTimeVar/bhpfilter/bHP.py
+++ PyTimeVar/PyTimeVar/bhpfilter/bHP.py
@@ -106,7 +106,7 @@ class BoostedHP:
         
         _, _, _, _, vBHP = self.results
         plt.figure(figsize=(12, 6))
-        plt.plot(x_vals, self.vY, label="True data", linewidth=2, color= 'black)
+        plt.plot(x_vals, self.vY, label="True data", linewidth=2, color= 'black')
         plt.plot(x_vals, vBHP, label="Estimated $\\beta_{0}$", linestyle="--", linewidth=2)
         
         plt.grid(linestyle='dashed')
